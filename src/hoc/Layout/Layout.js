c2648213f98b9e3cf5bceba6e3c4e8146a819329
Joe Kawai <kawaijoe@gmail.com>
2018-11-01 21:11:21 +0800

    Added boiler plate code
    Added Materialize-UI npm packages
    Added routing

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Navbar from './Navbar/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        {this.props.children}
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <React.Fragment>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.Fragment>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
