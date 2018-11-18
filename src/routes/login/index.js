9634147b385e567e8c60dfd0c0951526a95fbf93
Joe Kawai <kawaijoe@users.noreply.github.com>
2018-11-19 00:22:52 +0800

    Revert "LogIn, LogOut and SignUP working"

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,94 +1,82 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-import { Formik, Form, Field } from 'formik';
-import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types'; // lazy do :(
-
 import { logIn, logOut } from '../../store/actions/authActions';
-import TextField from '../../components/UI/TextField/TextField';
+//import { Redirect } from 'react-router-dom';
 
-
-const initialValues = {
-  email: '',
-  password: '',
-};
-
-const Login = ({
-  authError, auth, logIn, logOut, 
+const loginForm = ({
+  errors,
+  touched,
+  isSubmitting,
+  logOut,
+  authError
 }) => (
-  <React.Fragment>
-    <h1>Login!</h1>
-    <Formik
-      initialValues={initialValues}
-      validationSchema={Yup.object({
-        email: Yup.string().email('Email not valid').required('Email is required'),
-        password: Yup.string()
-          .min(8, 'Password must be 8 characters or longer')
-          .required('Password is required'),
-      })}
-      onSubmit={(values, { setErrors, setSubmitting, resetForm }) => {
-        setTimeout(() => {
-          // alert(JSON.stringify(values, null, 2));
-          // login user
-          logIn(values);
-          if (authError) {
-            setErrors({ form: authError });
-          } else {
-            resetForm();
-          }
-          setSubmitting(false);
-        }, 500);
-        console.log(values);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <Form onSubmit={handleSubmit}>
-          <TextField
-            required
-            name="email"
-            label="Email"
-            type="email"
-            onChange={handleChange}
-            values={values}
-            errors={errors}
-          />
-          <br />
-          <TextField
-            required
-            name="password"
-            label="Password"
-            type="password"
-            onChange={handleChange}
-            values={values}
-            errors={errors}
-          />
-          <br />
-          <br />
-          <Button type="submit" variant="contained" color="primary" disabled={isSubmitting}>
-            Submit
-          </Button>
-          <Button type="button" variant="contained" color="primary" href="/signIn" onClick={() => { logOut(); }}>
-            Logout
-          </Button>
-          <div>
-            {authError ? <font color="red">{authError}</font> : null}
-          </div>
-        </Form>
-      )}
-    </Formik>
-  </React.Fragment>
+  <Form>
+    <h1>LOGIN</h1>
+    <div>
+      {touched.email && errors.email && <p>{errors.email}</p>}
+      <Field type="email" name="email" placeholder="Enter Email" />
+    </div>
+    <div>
+      {touched.password && errors.password && <p>{errors.password}</p>}
+      <Field type="password" name="password" placeholder="Enter password" />
+    </div>
+    <Link to="/signup">Sign Up Now!</Link>
+    <div />
+    <div>
+      <button type="submit" disabled={isSubmitting}>
+        Submit
+      </button>
+    </div>
+    <div>
+      <button type="button" href="/signIn" onClick={logOut}>
+        Logout
+      </button>
+      <div>
+        {authError ? <font color="red">{authError}</font> : null}
+      </div>
+    </div>
+
+  </Form>
 );
+
+const Login = withFormik({
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || '',
+      password: password || '',
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email('Email not valid')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(8, 'Password must be 8 characters or longer')
+      .required('Password is required'),
+  }),
+
+  handleSubmit(values, {
+    resetForm, setErrors, setSubmitting, props,
+  }) {
+    setTimeout(() => {
+      // alert(JSON.stringify(values, null, 2)); // gives user a popup of the values
+      // login user
+      props.logIn(values);
+      if (props.authError) {
+        setErrors({ form: props.authError });
+      } else {
+        resetForm();
+      }
+      setSubmitting(false);
+    }, 2000);
+    console.log(values);
+  },
+})(loginForm);
+
 
 const mapStateToProps = (state) => {
   console.log(state);
@@ -98,23 +86,11 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = dispatch => ({
-  logIn: creds => dispatch(logIn(creds)),
-  logOut: () => dispatch(logOut()),
-});
-
-Login.propTypes = {
-  auth: PropTypes.object,
-  authError: PropTypes.string,
-  logIn: PropTypes.func,
-  logOut: PropTypes.func,
-};
-
-Login.defaultProps = {
-  auth: {},
-  authError: null,
-  logIn: () => {},
-  logOut: () => {},
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logIn: (creds) => dispatch(logIn(creds)),
+    logOut: () => dispatch(logOut()),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
